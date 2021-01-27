cc0ff9548ee1750e9fa8be3ac86d3588678a20f1
daviidy <yaodavidarmel@gmail.com>
2021-01-27 14:24:59 +0000

    Revert "tried to fix reference error"
    This reverts commit 1e788cc67bb32c6bab129e498d94393c05bdc95e.

--- src/components/todos/addTask.js
+++ src/components/todos/addTask.js
@@ -3,9 +3,9 @@ import addTaskToDom from './addTaskToDom'
 
 const addTask = (title, description, dueDate, priority) => {
   let todo = new Todo(title, description, dueDate, priority);
-  // const TaskId = "project-"+Date.now()
-  // localStorage.setItem(projectId, JSON.stringify(project))
-  // addTaskToDom(project.titleProp, projectId)
+  const TaskId = "project-"+Date.now()
+  localStorage.setItem(projectId, JSON.stringify(project))
+  addTaskToDom(project.titleProp, projectId)
 }
 
 export default addTask
