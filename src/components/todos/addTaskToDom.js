69c8f04e3c684632b9b9b5b25c1535a12d267cc0
daviidy <yaodavidarmel@gmail.com>
2021-01-28 14:36:21 +0000

    added methods for editing a task

--- src/components/todos/addTaskToDom.js
+++ src/components/todos/addTaskToDom.js
@@ -1,8 +1,16 @@
+import editTask from './editTask'
+
 const addTaskToDom =(project)=>{
 
+  const tasks= document.getElementById("tasks")
+
+   while(tasks.firstChild){
+       tasks.removeChild(tasks.lastChild)
+
+   }
+
   for(let i=0; i<project.length; i++){
-    console.log(project[i]._title)
-    
+
   const taskCard= document.getElementById("tasks")
   const ulElement= document.createElement("ul");
 
@@ -12,14 +20,21 @@ const addTaskToDom =(project)=>{
   const taskDiv= document.createElement("div");
   taskDiv.classList.add("ml-auto");
 
+  const linkEdit = document.createElement("a");
+  linkEdit.classList.add('pointer')
+  linkEdit.setAttribute("data-toggle","modal")
+  linkEdit.setAttribute("data-target","#editTask")
+  linkEdit.setAttribute('id', i)
+
   const iTagEdit= document.createElement("i");
-  iTagEdit.classList.add("fas", "fas-pencil-alt")
+  iTagEdit.classList.add("fas", "fa-pencil-alt")
 
   const iTagView= document.createElement("i");
   iTagView.classList.add("far", "fa-eye");
 
   // appending elements
-  taskDiv.append(iTagEdit);
+  taskDiv.append(linkEdit);
+  linkEdit.append(iTagEdit);
   taskDiv.append(iTagView);
 
   liElement.append(project[i]._title);
@@ -31,6 +46,16 @@ const addTaskToDom =(project)=>{
 
   }
 
+  const editTaskModal = document.getElementById('editTask')
+  const newTaskLink = document.getElementById('newTaskLink')
+  const projectId = newTaskLink.getAttribute('data-attribute')
+  const editTaskLinks = document.getElementsByClassName('fa-pencil-alt')
+  for (let i = 0; i < editTaskLinks.length; i++) {
+    editTaskLinks[i].parentElement.addEventListener('click', (event) => {
+      let taskId = editTaskLinks[i].parentElement.getAttribute('id')
+      editTask(projectId, i)
+    })
+  }
 
 }
 export default addTaskToDom
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import index from './components/projects/index'
 import showProject from "./components/projects/showProject"
 import addTask from './components/todos/addTask'
 
-//display all the projects
 const projects = index()
 Object.entries(projects).map(([key,value]) => {
   addProjectToDom(JSON.parse(value)._title, key)
@@ -22,12 +21,10 @@ buttonProject.addEventListener('click', (event) => {
   event.preventDefault()
   if (titleProject.value !== '') {
     addProject(titleProject.value)
-    console.log(`project added: ${titleProject.value}`)
   }
 })
 
 for (let i = 0; i < allProjects.length; i++) {
-  // console.log(allProjects[i])
   allProjects[i].addEventListener('click', (event) => {
     event.preventDefault()
     let id = allProjects[i].getAttribute('id')
@@ -40,16 +37,13 @@ const taskDescription= document.getElementById("description");
 const taskDate= document.getElementById("date");
 const taskPriority= document.getElementById("priority");
 const taskButton= document.getElementById("taskButton");
-// const taskLink= document.getElementById("projectID");
 
 
 taskButton.addEventListener("click", (event)=>{
   event.preventDefault()
   if(taskTitle.value!=="" && taskDescription.value!=="" && taskDate.value!==""&& taskPriority.value!==""){
-    console.log('it worked')
     const newTaskLink = document.getElementById('newTaskLink')
     const id = newTaskLink.getAttribute('data-attribute')
-    console.log(id)
     addTask(taskTitle.value, taskDescription.value, taskDate.value, taskPriority.value, id)
   }
 })
