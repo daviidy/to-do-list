0dc1b153737bf4c0a64e09ae5b93d9807877e5d2
daviidy <yaodavidarmel@gmail.com>
2021-01-29 08:00:27 +0000

    added methods for showing a specific task in a modal

--- src/components/todos/addTaskToDom.js
+++ src/components/todos/addTaskToDom.js
@@ -1,4 +1,5 @@
 import editTask from './editTask'
+import showTask from './showTask'
 
 const addTaskToDom =(project)=>{
 
@@ -24,10 +25,14 @@ const addTaskToDom =(project)=>{
   linkEdit.classList.add('pointer')
   linkEdit.setAttribute("data-toggle","modal")
   linkEdit.setAttribute("data-target","#editTask")
-  linkEdit.setAttribute('id', i)
 
   const iTagEdit= document.createElement("i");
   iTagEdit.classList.add("fas", "fa-pencil-alt")
+
+  const linkShow = document.createElement("a");
+  linkShow.classList.add('pointer')
+  linkShow.setAttribute("data-toggle","modal")
+  linkShow.setAttribute("data-target","#showTask")
 
   const iTagView= document.createElement("i");
   iTagView.classList.add("far", "fa-eye");
@@ -35,7 +40,8 @@ const addTaskToDom =(project)=>{
   // appending elements
   taskDiv.append(linkEdit);
   linkEdit.append(iTagEdit);
-  taskDiv.append(iTagView);
+  taskDiv.append(linkShow)
+  linkShow.append(iTagView);
 
   liElement.append(project[i]._title);
   liElement.append(taskDiv);
@@ -46,14 +52,19 @@ const addTaskToDom =(project)=>{
 
   }
 
-  const editTaskModal = document.getElementById('editTask')
   const newTaskLink = document.getElementById('newTaskLink')
   const projectId = newTaskLink.getAttribute('data-attribute')
   const editTaskLinks = document.getElementsByClassName('fa-pencil-alt')
   for (let i = 0; i < editTaskLinks.length; i++) {
     editTaskLinks[i].parentElement.addEventListener('click', (event) => {
-      let taskId = editTaskLinks[i].parentElement.getAttribute('id')
       editTask(projectId, i)
+    })
+  }
+
+  const viewTaskLinks = document.getElementsByClassName('fa-eye')
+  for (let i = 0; i < viewTaskLinks.length; i++) {
+    viewTaskLinks[i].parentElement.addEventListener('click', (event) => {
+      showTask(projectId, i)
     })
   }
 
